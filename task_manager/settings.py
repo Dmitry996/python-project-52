e47df3a8d72837904684d59a2d414e6319a533b4
dmitry_rodin <dimoon-96@yandex.ru>
2024-06-05 20:53:05 +0700

    fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -141,17 +141,10 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_URL = 'static/'
 
 if not DEBUG:
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
