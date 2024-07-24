b9e55eb84e889f12aabbe63cd087f97f3cb1b250
dmitry_rodin <dimoon-96@yandex.ru>
2024-07-24 15:08:52 +0700

    fix task

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import ListTasks, CreateTask, UpdateTask, DeleteTask, ViewTask
+from .views import ListTasksView, CreateTaskView, UpdateTaskView, DeleteTaskView, TaskView
 
 
 urlpatterns = [
-    path('', ListTasks.as_view(), name='tasks'),
-    path('<int:pk>/', ViewTask.as_view(), name='view_task'),
-    path('create/', CreateTask.as_view(), name='create_task'),
-    path('<int:pk>/update/', UpdateTask.as_view(), name='update_task'),
-    path('<int:pk>/delete/', DeleteTask.as_view(), name='delete_task'),
+    path('', ListTasksView.as_view(), name='tasks'),
+    path('<int:pk>/', TaskView.as_view(), name='view_task'),
+    path('create/', CreateTaskView.as_view(), name='create_task'),
+    path('<int:pk>/update/', UpdateTaskView.as_view(), name='update_task'),
+    path('<int:pk>/delete/', DeleteTaskView.as_view(), name='delete_task'),
 ]
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,23 +1,25 @@
+from django.views.generic import CreateView, ListView, DeleteView, UpdateView
 from django.shortcuts import render
 from .models import Task
 from .forms import TaskForm
-
+from django.utils.translation import gettext_lazy as _
+from django.contrib.messages.views import SuccessMessageMixin
 
 class TaskView():
     pass
 
 
-class ListTasksView():
+class ListTasksView(ListView):
     pass
 
 
-class CreateTaskView():
+class CreateTaskView(CreateView):
     pass
 
 
-class UpdateTaskView():
+class UpdateTaskView(UpdateView):
     pass
 
 
-class DeleteTaskView():
+class DeleteTaskView(DeleteView):
     pass
